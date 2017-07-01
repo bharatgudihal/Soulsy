8b8078f953453f9f4c86f590d1ae0f7c1586b965
JPsLucky13 <jeanpaulpeschard43@gmail.com>
2017-07-01 14:03:25 -0600

    Added Check left joystick condition and Bbutton check condition

---
+++ Assets/Scripts/Conditions/CheckLeftJoystick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckLeftJoystick : MonoBehaviour{
+
+    [Range(0,1)]
+    public  float walkValue;
+
+    public enum Comparison
+    {
+        Equal,
+        NotEqual,
+        Greater,
+        GreaterEqual,
+        Lower,
+        LowerEqual
+    };
+
+    public Comparison type = Comparison.Equal;
+
+    private bool LeftJoystickXaxisValue()
+    {
+        if(type == Comparison.Equal)
+            return ControllerInput.GetLeftAnalogStickXValue() == walkValue;
+
+        if (type == Comparison.NotEqual)
+            return ControllerInput.GetLeftAnalogStickXValue() != walkValue;
+
+        if(type == Comparison.Greater)
+            return ControllerInput.GetLeftAnalogStickXValue() > walkValue;
+
+        if (type == Comparison.GreaterEqual)
+            return ControllerInput.GetLeftAnalogStickXValue() >= walkValue;
+
+        if (type == Comparison.Lower)
+            return ControllerInput.GetLeftAnalogStickXValue() < walkValue;
+
+        if (type == Comparison.LowerEqual)
+            return ControllerInput.GetLeftAnalogStickXValue() <= walkValue;
+
+        else
+            return true;
+    }
+}
---
+++ Assets/Scripts/Controller/ControllerInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput{
+
+    public static float GetLeftAnalogStickXValue()
+    {
+        return Input.GetAxis("Walk");
+    }
+
+    public static bool GetBButtonValue()
+    {
+        return Input.GetButton("BButton");
+    }
+    
+     
+}
