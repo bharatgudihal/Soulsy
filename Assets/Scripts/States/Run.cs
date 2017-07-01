7184fbcfc8e244f0bf5f7ef7452e6e8e58903919
JPsLucky13 <jeanpaulpeschard43@gmail.com>
2017-07-01 16:11:12 -0600

    Added Run State

---
+++ Assets/Scripts/States/Run.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : State {
+
+    Vector2 runVect;
+
+    // Use this for initialization
+    void Start()
+    {
+        runVect = new Vector2(GetComponent<Stats>().runSpeed, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckConditions();
+        float direction = Mathf.Sign(ControllerInput.GetLeftAnalogStickXValue());
+        transform.Translate(runVect * direction);
+
+    }
+}
